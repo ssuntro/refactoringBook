44e20b4d3d46c35fe933de0ab7ce28c1fdec6ac3
Siroratt Suntronsuk <ann@odds.team>
2020-02-15 21:40:54 +0700

    split statement volumeCredits

--- refactoringBook.js
+++ refactoringBook.js
@@ -2,14 +2,14 @@ function statement (invoice, plays) {
     let totalAmount = 0
     let result = 'Statement for ${invoice.customer}\n'
 
-    
-    let volumeCredits = totalVolumeCredits()
     for(let perf of invoice.performances) {
         //print line for this order
         result += '  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n'
         totalAmount += amountFor(perf)
     }
     result += 'Amount owed is ${usd(totalAmount)}\n'
+
+    let volumeCredits = totalVolumeCredits()
     result += 'You earned ${volumeCredits} credits\n'
     return result
 

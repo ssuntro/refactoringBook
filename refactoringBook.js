652db095558ac5c6bc4eec5ced62f0f7a4a3558e
Siroratt Suntronsuk <ann@odds.team>
2020-02-15 21:02:12 +0700

    extract function format(aNumber)

--- refactoringBook.js
+++ refactoringBook.js
@@ -2,18 +2,21 @@ function statement (invoice, plays) {
     let totalAmount = 0
     let volumeCredits = 0
     let result = 'Statement for ${invoice.customer}\n'
-    const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
     for(let perf of invoice.performances) {
         
         volumeCredits += volumeCreditsFor(perf)
 
         //print line for this order
-        result += '  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n'
+        result += '  ${playFor(perf).name}: ${format(amountFor(perf))} (${perf.audience} seats)\n'
         totalAmount += amountFor(perf)
     }
-    result += 'Amount owed is ${format(totalAmount / 100)}\n'
+    result += 'Amount owed is ${format(totalAmount)}\n'
     result += 'You earned ${volumeCredits} credits\n'
     return result
+
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100)
+    }
 
     function volumeCreditsFor(aPerformance) {
         let result = 0
